fdd75a9c95b74d0af0d66aeaadc986885f62922c
Chinmayi <chinmayikg33@gmai.com git config --global user.name Chinmayigit config --global user.email chinmayikg33@gmail.com>
2025-06-20 23:52:22 +0530

    Initial commit for Singleton pattern

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        Singleton obj1 = Singleton.getInstance();
+        obj1.showMessage();
+
+        Singleton obj2 = Singleton.getInstance();
+        obj2.showMessage();
+
+        if (obj1 == obj2) {
+            System.out.println("Both objects are the same instance.");
+        } else {
+            System.out.println("Different instances (wrong implementation).");
+        }
+    }
+}
